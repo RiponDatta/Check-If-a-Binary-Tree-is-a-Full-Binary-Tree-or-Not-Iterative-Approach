6d8b6d3ed09ac3ca690849ce7973a82e43b47033
Ripon Datta <ripondatta.us@gmail.com>
2019-06-11 23:06:34 -0700

    Add project files.

---
+++ BinaryTree is a Full BinaryTree or Not Iterative/BinaryTree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree_is_a_Full_BinaryTree_or_Not_Iterative
+{
+    public class BinaryTree
+    {
+        public Node Root;
+
+        public bool IsFullBinaryTree()
+        {
+            if(Root == null)
+            {
+                return true;
+            }
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(Root);
+
+            while(queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+                if(node.Left == null && node.Right == null)
+                {
+                    continue;
+                }
+                if(node.Left == null || node.Right == null)
+                {
+                    return false;
+                }
+
+                queue.Enqueue(node.Left);
+                queue.Enqueue(node.Right);
+            }
+            return true;
+        }       
+    }
+}
